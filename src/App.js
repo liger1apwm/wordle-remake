090064d04840f4e274baaf19b774d8e0fc631fb3
BoraCeci <83080102+BoraCeci@users.noreply.github.com>
2022-05-01 16:15:21 -0400

    board components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import Board from './Components/Board';
+import {createContext, useState} from "react";
+import { boardDefault } from './Components/Words';
+
+export const AppContext = createContext();
 
 
 function App(){
+  const [board, setBoard] = useState(boardDefault);
 
 return ( 
   <div className='App'>
     <nav>
       <h1> Wordle </h1>
     </nav>
+    <AppContext.Provider value = {{board, setBoard}}>
+      <Board />
+    </AppContext.Provider>
   </div>
 );
 

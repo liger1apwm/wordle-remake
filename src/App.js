0a3d11dfe86895dc50ba0a249ba30e6e427f2137
BoraCeci <83080102+BoraCeci@users.noreply.github.com>
2022-05-02 22:44:14 -0400

    adding different components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import './App.css';
-import Board from './Components/Board';
-import {createContext, useState} from "react";
-import { boardDefault } from './Components/Words';
+import Grid from "./Components/Grid";
 
-export const AppContext = createContext();
 
 
 function App(){
-  const [board, setBoard] = useState(boardDefault);
 
 return ( 
   <div className='App'>
     <nav>
       <h1> Wordle </h1>
     </nav>
-    <AppContext.Provider value = {{board, setBoard}}>
-      <Board />
-    </AppContext.Provider>
+      < Grid />
   </div>
 );
 
---
+++ src/Components/Grid-square.js
@@ -0,0 +1,9 @@
+import "../Styles/Grid.css"
+
+function Square(){
+    return(
+     <div className="square"> T </div>
+    )
+}
+
+export default Square
